aa1f6c9b374a8808b39aba561bf9d6d57ddacd3a
Danilamaimeskul <59773922+Danilamaimeskul@users.noreply.github.com>
2021-03-30 01:42:23 +0300

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,82 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(a){
+    if(a === false){
+      this.typeMachine = false;
+    }
+    else{
+      this.typeMachine = true;
+    }
+    }
+  encrypt(stringParam, stringKey) {
+    if(stringParam== undefined || stringKey == undefined) throw new Error('THROWN');
+    if(typeof(stringParam) != "string" || typeof(stringKey) != "string") throw new Error('THROWN');
+    let y = 0;
+    let str = [];
+    stringParam = stringParam.split("");
+    stringKey = stringKey.split("");
+    for(let i = 0; i < stringParam.length; i++){
+      stringParam[i] = stringParam[i].toLowerCase();
+    }
+    for(let i = 0; i < stringKey.length; i++){
+      stringKey[i] = stringKey[i].toLowerCase();
+    }
+    for(let i = 0; i < stringParam.length; i++){
+      if(stringParam[i].charCodeAt() < 97 || stringParam[i].charCodeAt() > 122){
+      str[i] = stringParam[i];
+      --y;
+    }
+    else if(stringParam[i].charCodeAt() + stringKey[y].charCodeAt() - 97 > 122){
+      str[i] = String.fromCharCode(stringParam[i].charCodeAt() + stringKey[y].charCodeAt()-123)
+    }else{
+      str[i] = String.fromCharCode(stringParam[i].charCodeAt() + stringKey[y].charCodeAt()-97)
+    }
+    if(str[i] === '`') str[i] = "z";
+    ++y;
+    if(y === stringKey.length) y = 0;
+    }
+    for(let i = 0; i < str.length; i++){
+      str[i] = str[i].toUpperCase();
+    }
+    if(this.typeMachine === false) str = str.reverse();
+    str = str.join("");
+    return str;
+    }   
+  decrypt(stringParam, stringKey) {
+    if(stringParam == undefined || stringKey == undefined) throw new Error('THROWN');
+    if(typeof(stringParam) != "string" || typeof(stringKey) != "string")throw new Error('THROWN');
+    let y = 0;
+    let str = [];
+    stringParam = stringParam.split("");
+    stringKey = stringKey.split("");
+    for(let i = 0; i < stringParam.length; i++){
+      stringParam[i] = stringParam[i].toLowerCase();
+    }
+    for(let i = 0; i < stringKey.length; i++){
+      stringKey[i] = stringKey[i].toLowerCase();
+    }
+    for(let i = 0; i < stringParam.length; i++){
+      if(stringParam[i].charCodeAt() < 97 || stringParam[i].charCodeAt() > 122){
+        str[i] = stringParam[i];
+        --y
+      }
+      else if(stringParam[i].charCodeAt() - stringKey[y].charCodeAt() + 97 >= 96){
+        str[i] = String.fromCharCode(stringParam[i].charCodeAt() - stringKey[y].charCodeAt() + 97)
+      }else{
+        str[i] = String.fromCharCode(stringParam[i].charCodeAt() - stringKey[y].charCodeAt() + 123)
+      }
+      if(str[i] === '`') str[i] = "z";
+      ++y;
+      if(y === stringKey.length) y = 0;
+    }
+    if(this.typeMachine === false) stringParam.reverse()
+    for(let i = 0; i < str.length; i++){
+      str[i] = str[i].toUpperCase();
+    }
+    if(this.typeMachine === false) str.reverse();
+    str = str.join("");
+    return str;
   }
 }
 
